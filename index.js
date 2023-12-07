662212f9e6a5f6de74e26fe5ebccf6c900747db5
ivonneandrea <ivonne.bn@gmail.com>
2023-12-07 16:52:25 -0300

    adding js

---
+++ index.js
@@ -0,0 +1,46 @@
+//elementos del Dom
+const precioSpan = document.querySelector(".precio-inicial");
+const total = document.querySelector(".valor-total");
+const sumar = document.querySelector("#btn-sumar");
+const restar = document.querySelector("#btn-restar");
+const cantidad = document.querySelector(".cantidad");
+
+
+//variables
+let precio = 400000;
+let totalValue = 0;
+precioSpan.innerHTML= precio;
+total.innerHTML= totalValue;
+
+
+//funciones
+const sumarPrecio = () => {
+    
+    precio += 400000;
+    total.innerHTML= precio;
+    cantidad.innerHTML= Number(cantidad.innerHTML) + 1;
+
+};
+
+const restarPrecio = () => {
+    precio -= 400000;
+    total.innerHTML= precio;
+    cantidad.innerHTML= Number(cantidad.innerHTML) - 1;
+
+};
+
+ //eventos 
+sumar.addEventListener("click", sumarPrecio => {
+    precio += 400000;
+    total.innerHTML = precio;
+    cantidad.innerHTML= Number(cantidad.innerHTML) + 1;
+});
+
+
+restar.addEventListener("click", restarPrecio => {
+    precio -= 400000;
+    total.innerHTML = precio;
+    cantidad.innerHTML= Number(cantidad.innerHTML) - 1;
+});
+
+
