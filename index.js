2fb1904bebcba27c55e0dd625610f9e4141bba9f
ivonneandrea <ivonne.bn@gmail.com>
2023-12-08 20:55:33 -0300

    adding js

--- index.js
+++ index.js
@@ -5,42 +5,34 @@ const sumar = document.querySelector("#btn-sumar");
 const restar = document.querySelector("#btn-restar");
 const cantidad = document.querySelector(".cantidad");
 
-
 //variables
-let precio = 400000;
-let totalValue = 0;
-precioSpan.innerHTML= precio;
-total.innerHTML= totalValue;
-
+    let precio = 400000;
+    let totalValue = 0;
+    precioSpan.innerHTML = precio;
+    total.innerHTML = totalValue;
 
 //funciones
 const sumarPrecio = () => {
-    
-    precio += 400000;
-    total.innerHTML= precio;
-    cantidad.innerHTML= Number(cantidad.innerHTML) + 1;
-
+  precio += 400000;
+  total.innerHTML = precio;
+  cantidad.innerHTML = Number(cantidad.innerHTML) + 1;
 };
 
 const restarPrecio = () => {
-    precio -= 400000;
-    total.innerHTML= precio;
-    cantidad.innerHTML= Number(cantidad.innerHTML) - 1;
-
+  precio -= 400000;
+  total.innerHTML = precio;
+  cantidad.innerHTML = Number(cantidad.innerHTML) - 1;
 };
 
- //eventos 
-sumar.addEventListener("click", sumarPrecio => {
-    precio += 400000;
-    total.innerHTML = precio;
-    cantidad.innerHTML= Number(cantidad.innerHTML) + 1;
+//eventos
+sumar.addEventListener("click", () => {
+  let currentQuantify = Number(cantidad.innerHTML) + 1;
+  cantidad.innerHTML = currentQuantify;
+  total.innerHTML = precio * currentQuantify;
 });
 
-
-restar.addEventListener("click", restarPrecio => {
-    precio -= 400000;
-    total.innerHTML = precio;
-    cantidad.innerHTML= Number(cantidad.innerHTML) - 1;
+restar.addEventListener("click", () => {
+  let currentQuantify = Number(cantidad.innerHTML) - 1;
+  cantidad.innerHTML = currentQuantify;
+  total.innerHTML = precio * currentQuantify;
 });
-
-
